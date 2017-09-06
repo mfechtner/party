e64fc34237239a027e946b143aced8bb495d015d
mfechtner <moritz.fechtner@source-tree.com>
2017-09-06 21:35:09 +0200

    Whoop

--- vis.py
+++ vis.py
@@ -13,21 +13,22 @@ def run():
         rval = False
 
     while rval:
-        gray_image = cv2.cvtColor( frame, cv2.COLOR_RGB2RGBA);
-        #gray_image = frame
+        gray_image = cv2.cvtColor( frame, cv2.COLOR_RGB2GRAY);
 
         faces = face_cascade.detectMultiScale(
             gray_image,
             scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30)
+            minNeighbors=3,
+            minSize=(50, 50),
+            maxSize=(400, 400),
         )
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(gray_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imwrite('megusta.png', frame)
 
 
-        cv2.imshow("preview", gray_image)
+        cv2.imshow("preview", frame)
         rval, frame = vc.read()
         key = cv2.waitKey(1)
         if key == 27: # exit on ESC
