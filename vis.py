a06ca2f259a679d283cf04f1327024e21a6dc020
mfechtner <moritz.fechtner@source-tree.com>
2017-09-14 17:04:15 +0200

    Drop Circle

--- vis.py
+++ vis.py
@@ -7,10 +7,19 @@ def overlay(dest, source, posx, posy, S=None, D=None):
     S = S or (0.5, 0.5, 0.5, 0.5)
     D = D or (0.5, 0.5, 0.5, 0.5)
 
-    
+circles = []
+
+def draw_cirlce(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(event)
+        circles.append((x, y))
 
 def run():
-    cv2.namedWindow("preview")
+    window_name = "preview"
+
+    cv2.namedWindow(window_name)
+    cv2.setMouseCallback(window_name, draw_cirlce)
+
     vc = cv2.VideoCapture(0)
 
     layers = [None, None, None]
@@ -18,27 +27,17 @@ def run():
 
     while True:
         ret, image = vc.read()
-        width, height, colors = image.shape
-        scale = 640.0 / width
-        #image = cv2.resize(image, (0,0), fx=scale, fy=scale)
-        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray,50,150, apertureSize = 3)
 
-        layers[offset%len(layers)] = edges
+        for x, y in circles:
+            print(x, y)
+            cv2.circle(image,(x,y),100,(255,0,0),-1)
 
-        for layer in layers:
-            if layer is not None:
-                continue
-            edges = numpy.concatenate((layer, edges))
+        cv2.imshow(window_name, image)
 
-        cv2.imshow('Camera stream', edges)
-        
-        offset += 1
-
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    cv2.destroyWindow("preview")
+    cv2.destroyWindow(window_name)
 
 if __name__ == '__main__':
     run()
