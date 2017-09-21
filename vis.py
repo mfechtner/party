2fdf150f2035521938ac784ba535b10802fbf126
mfechtner <moritz.fechtner@source-tree.com>
2017-09-21 12:26:51 +0200

    Top

--- vis.py
+++ vis.py
@@ -1,6 +1,9 @@
 import cv2, numpy, time
 from imutils import perspective
 import math
+import screeninfo
+
+from datetime import datetime
 
 class MouseHandler(object):
     def __init__(self, videos=[]):
@@ -39,30 +42,39 @@ class Video(object):
     def __init__(self, video_capture, points=None):
         self.video_capture = video_capture
         self.points = points
+        self.frame = 0
+        self.frame_count = self.video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    def step(self):
+        self.frame += 1
+        if self.frame >= self.frame_count:
+            self.frame = 0
+            self.video_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
 
 def run():
     window_name = "preview"
+    fps = 25
+    time_per_frame = 1000 / fps
 
     videos = [
+        Video(cv2.VideoCapture('Test_01.mp4')),
         Video(cv2.VideoCapture('Test_01.mp4')),
         Video(cv2.VideoCapture('Test_01.mp4'))
     ]
 
     handler = MouseHandler(videos)
     
-    cv2.namedWindow(window_name)
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.setMouseCallback(window_name, handler.callback)
-    
 
 
-
-    dim  = (700,858,3)
+    dim  = (600,800,3)
     sh,sw,d = dim
 
     while True:
+        start = datetime.now()
         canvas = numpy.zeros((sh,sw,d), numpy.uint8)
-
         x_ = 0.0
 
         for video in videos:
@@ -100,6 +112,7 @@ def run():
             image = cv2.warpPerspective(image, M, dsize=(sw,sh))
             y_offset = 0
             x_offset = 0
+            video.step()
 
             canvas = cv2.addWeighted(canvas,1,image,1,0)
             #canvas[y_offset:y_offset+image.shape[0], x_offset:x_offset+image.shape[1]] = image
@@ -107,11 +120,17 @@ def run():
         if handler.edit:
             for video in videos:
                 for p in video.points:
-                    cv2.circle(canvas, tuple(p), 10, (0,0,255), -1)
+                    cv2.circle(canvas, tuple(p), 5, (0,0,255), -1)
+                    cv2.circle(canvas, tuple(p), 1, (255,255,255), -1)
         
         cv2.imshow(window_name, canvas)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        end = datetime.now()
+
+        diff = (end - start).microseconds / 1000
+        sleep_time = int(max(time_per_frame - diff, 1))
+
+        if cv2.waitKey(sleep_time) & 0xFF == ord('q'):
             break
 
     cv2.destroyWindow(window_name)
